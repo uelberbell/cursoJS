06b8820fee62c81d59a47ef3eb6e0fa3ab41c9f2
uelberbell <uelberbell@gmail.com>
2021-03-29 12:52:46 -0300

    Condições
    Hoje a gente aprendeu sobre condicionais. É importante para fazer com que o código entre em alguns blocos ou não.

---
+++ aula11/exe008.js
@@ -0,0 +1,8 @@
+var vel = 64.90
+console.log('teste de impressão')
+if (vel == 64.90){ //consição simples
+    console.log(`A velocida ultrapassada foi de: ${vel}Km`)
+}
+else{
+    console.log(`A velocidade: ${vel}Km não é igual a: 64.90Km`)
+}
