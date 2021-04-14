485c813d695483852bfc00298183496543816a45
uelberbell <72271381+uelberbell@users.noreply.github.com>
2021-04-14 13:31:42 -0300

    WHILE e DO WHILE
    Hoje foi interessante aprender estrutura de repetição em JS

---
+++ aula13/ambiente.js
@@ -0,0 +1,16 @@
+
+var c = 1
+do{ //faça:
+    console.log(`Passo ${c}`)
+    c++ // Importante sempre acrescentar um ++(mais 1) para que o looping não fique infinito.
+    
+}while( c <= 20) // Enquanto c for menor igual a 10
+
+
+
+/*
+var c = 1
+while ( c <= 10){ // Enquanto c for menor igual a 10
+    console.log(`Passo ${c}`)
+    c++ // Importante sempre acrescentar um ++(mais 1) para que o looping não fique infinito.
+}*/
