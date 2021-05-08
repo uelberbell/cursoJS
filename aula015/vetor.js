636d705552250bfca6ae81a685c0972ff10aabfa
uelberbell <72271381+uelberbell@users.noreply.github.com>
2021-05-08 15:59:41 -0300

    Update vetor.js
    Up modificação aula 15

--- aula015/vetor.js
+++ aula015/vetor.js
@@ -1,2 +1,6 @@
 let num = [5, 8, 4]
+//num[3] = 3 //Forma de adicionar um valor após o vetor estar criado. Mas neste caso estamos definindo a posição
+num.push(1) // Neste outro caso estamos dizendo o valor que queremos inserir, mas o Java Script cuida da alocação do valor.
+num.sort()
 console.log(`Dados dentro da lista: ${num}`)
+console.log(`Nossa lista tem: ${num.length} posições`)
