94852b544dc22e645501dc1d33febb1557f9a8fc
uelberbell <72271381+uelberbell@users.noreply.github.com>
2021-05-14 11:02:21 -0300

    Contador Crescente
    Criado para adicionar valores em uma planilha.

---
+++ aula016/contar.js
@@ -0,0 +1,7 @@
+     for (let cont =1; cont <=100 ; cont++){
+        console.log(`CD - 00${cont},`)
+    }
+
+
+//Para fazer esse contador, não precisamos necessáriamente de uma função.
+//Criando uma variável, definimos um valor, damos uma condição e adcionamos valor após a condição.
