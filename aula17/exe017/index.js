6028791a90dab82d5940493c05e5079243e11e62
uelberbell <72271381+uelberbell@users.noreply.github.com>
2021-05-24 17:11:43 -0300

    Erro grave corrigido
    Claro que as opções adicionadas não deveriam aparecer, pois o "option" estava escrito errado: <opition></opition>

--- aula17/exe017/index.js
+++ aula17/exe017/index.js
@@ -1,8 +1,9 @@
 let num = document.getElementById('txtnum')
+let seletor = document.getElementById('seletor')
 let res = document.getElementById('imprimir')
 let valores = []
 
-function isNumber(n){
+function isNumero(n){
     if (Number(n) >=1 && (Number(n)) <= 100){
         return true
     } else{
@@ -19,8 +20,11 @@ function isLista(n, l){
 }
 
 function adicionar(){ 
-    if (isNumber(num.value) && !isLista (num.value, valores)){
+    if (isNumero(num.value) && !isLista (num.value, valores)){
         valores.push(Number(num.value))
+        let dados = document.createElement(`option`)
+        dados.innerText = (`Valor ${num.value} adicionado na posição: ${valores.length}`)
+        seletor.appendChild(dados)
         res.innerHTML = ''
         console.log(`${valores}`)
 
@@ -33,6 +37,29 @@ function adicionar(){
 }
 
 function verificar(){
-    res.innerHTML = `O Total de valores adiconado é de <strong>${valores.length}</strong>`// Length usamos para pegar o tamanho do array. Strong deixamos em negrito
+    if(valores.length == 0){
+        alert('Você não pode verificar valores sem adicionar nenhum dado!') 
+    } else{
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for(let pos in valores){
+            soma += valores[pos]
+            if(valores[pos] > maior) //São dois IF diretos sem adição de chaves porque é necessário que das duas ações sejam feitas
+                maior = valores[pos]
+            if(valores[pos] < menor)
+                menor = valores[pos]  
+        }
+            media += soma /5
+        res.innerHTML += `O Total de valores adiconado é de <strong>${total}</strong><br>`// Length usamos para pegar o tamanho do array. Strong deixamos em negrito
+        res.innerHTML += `O total dos valores somados é: ${soma}<br>`
+        res.innerHTML += `A mádia dos valores é: ${media}<br>` 
+        res.innerHTML += `O maior valor adicionado na lista É: <strong>${maior}</strong><br>`
+        res.innerHTML += `O menor valor adicionado na lista é: <strong>${menor}</strong>`
+    
+    }
+   
     
 }
